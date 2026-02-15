f7ebba1e1b8b61e878e2e0c46e3cc4c8a69b6538
echa-wew <prasetya.inc@gmail.com>
2026-02-15 19:54:19 +0700

    Fix base path for GitHub Pages navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://pancakarsa.my.id',
+	site: 'https://echa-wew.github.io',
+	base: '/pancakarsa-blog',
 	integrations: [mdx(), sitemap()],
 });
