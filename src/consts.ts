66b36fd1abde9c02b93bedd61d9b6c148f53f2c6
echa-wew <prasetya.inc@gmail.com>
2026-02-15 17:55:46 +0700

    Initial setup: configure site title and domain

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// You can import this data from anywhere in your site by using the `import` statement.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pancakarsa';
+export const SITE_DESCRIPTION = 'Cerita, pengalaman hidup, insight teknologi, engineering, dan pemikiran strategis.';
