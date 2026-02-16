f151562fdcb59c447c9cb65025875a258be29b72
echa-wew <prasetya.inc@gmail.com>
2026-02-16 13:07:42 +0700

    Update site URL to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://echa-wew.github.io',
-	base: '/pancakarsa-blog',
+	site: 'https://pancakarsa.my.id',
 	integrations: [mdx(), sitemap()],
 });
