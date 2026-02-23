5f7fa9c19435408b4b2c4f2f92d530b574b6e39b
echa-wew <prasetya.inc@gmail.com>
2026-02-24 06:14:15 +0700

    update content config kategori

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
-			category: z.enum(['teknologi', 'pengalaman', 'pemikiran', 'engineering', 'cerita']),
+			category: z.enum(['teknologi', 'pengalaman', 'pemikiran', 'engineering', 'cerita', 'tips&trik']),
 			tags: z.array(z.string()).default([]),
 		}),
 });
